02e761566c0c4e48122ceafa9eb1679b54accea9
Devy Franco Cantalupo <devycantalupo@outlook.com>
2023-10-11 16:26:31 +0200

    First Commit

---
+++ src/esecizio4di2.java
@@ -0,0 +1,10 @@
+public class esecizio4di2 {
+    public static int media(int a, int b, int c){
+        int media = (a + b + c) / 3;
+        return media;
+    }
+    public static void main(String[] args) {
+        int risultato = media(3,6,91);
+        System.out.println("risultato = " + risultato);
+    }
+}
